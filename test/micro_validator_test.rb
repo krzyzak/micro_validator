452aa100a12faac38ed704be8b31e8c691d34634
Tomek Kalinowski <tki.wroc@gmail.com>
2013-08-05 11:32:22 +0200

    [#1] Fixed micro_validator tests.

--- test/micro_validator_test.rb
+++ test/micro_validator_test.rb
@@ -32,17 +32,20 @@ module MicroValidator
 
     def test_should_not_pass_validation
       MyKlass.validate :testing, :successful
+
       assert !@klass.valid?
     end
 
     def test_should_pass_validation
       MyKlass.validate :successful
-      @klass.valid?
+
+      assert @klass.valid?
     end
 
     def test_should_return_all_errors
       MyKlass.validate :testing, :another, :successful, :different_field
       @klass.valid?
+
       assert_equal ({ testing: ["Something failed...", "Another failure!"],
                       different_field: ["It's different this time."]} ),
                    @klass.errors.all
